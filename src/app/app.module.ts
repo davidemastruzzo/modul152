306f1188f4bf2d5c174b5a4d60427842a5595a26
davidemastruzzo <mastruzzodavide@gmail.com>
2019-06-04 08:08:54 +0200

    add material icons and basic slideshow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MusicplayyerComponent } from './musicplayyer/musicplayyer.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+import { IconComponent } from './general/icon/icon.component';
+import { IconButtonComponent } from './general/icon-button/icon-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MusicplayyerComponent,
+    HomepageComponent,
+    SlideshowComponent,
+    IconComponent,
+    IconButtonComponent
   ],
   imports: [
     BrowserModule,
